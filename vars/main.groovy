da2e7effffd1c8d891e5187ba80669616fd105be
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-17 21:42:04 +0530

    Create main.groovy

---
+++ vars/main.groovy
@@ -0,0 +1,8 @@
+class Example {
+   static void main(String[] args) {
+      Date date = new Date(100);
+      
+      // display time and date using toString()
+      System.out.println(date.toString());
+   } 
+}
