9386a18864785a8767b461fcfc75cc2b7914bd42
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-17 22:20:38 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 
-def person = [name: 'Guillaume', age: 36]
-assert "$person.name is $person.age years old" == 'Guillaume is 36 years old'
+def person = [name: 'Guillaume']
+assert "$person.name
