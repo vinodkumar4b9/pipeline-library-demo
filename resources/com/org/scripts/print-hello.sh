a3c3ba91b84585ae4ce6c73087a125f471a70794
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-18 23:27:29 +0530

    Update print-hello.sh

--- resources/com/org/scripts/print-hello.sh
+++ resources/com/org/scripts/print-hello.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-echo "Hello, $1"
+now="$(date)"
+printf "Current date and time %s\n" "$now"
+ 
+now="$(date +'%d/%m/%Y')"
+printf "Current date in dd/mm/yyyy format %s\n" "$now"
+ 
+echo "Starting backup at $now, please wait..."
+# command to backup scripts goes here
+# ...
