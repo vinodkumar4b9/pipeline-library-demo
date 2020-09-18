2ec065fbeab89fcee813e37cb6e73c7ac19d4aa4
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-18 22:27:31 +0530

    Update doSomething.groovy

--- vars/doSomething.groovy
+++ vars/doSomething.groovy
@@ -3,6 +3,5 @@
 def call(args) {
     def scriptDir = WORKSPACE + '@libs/pipeline-library-demo'
     sh "chmod +x do-something.sh"
-    sh "cp  /var/lib/jenkins/workspace/vinod-latest@libs/pipeline-library-demo/do-something.sh /var/lib/jenkins/workspace/vinod-latest"
     sh "$scriptDir/do-something.sh $args"
 }
