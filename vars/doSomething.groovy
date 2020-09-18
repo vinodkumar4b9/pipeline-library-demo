8227fdf09a33479b20148c967ec19d63e770eb6f
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-18 22:08:31 +0530

    Update doSomething.groovy

--- vars/doSomething.groovy
+++ vars/doSomething.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(args) {
-    def scriptDir = WORKSPACE + '@libs/pipeline-library-demo'
+    def scriptDir = WORKSPACE + '@libs/pipeline-library-demo'\
+    chmod 777 do-something.sh
     sh "$scriptDir/do-something.sh $args"
 }
