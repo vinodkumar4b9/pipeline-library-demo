1046bb4ecb5c4b67a60602e2cc8918441e32b93a
Mandalapu <vmandalapu@deloitte.com>
2020-09-18 16:23:04 +0530

    added

--- vars/printHello.groovy
+++ vars/printHello.groovy
@@ -1,5 +1,5 @@
 def call(Map parameters = [:]) {
-    def printHelloScript = libraryResource 'com/cleverbuilder/print-hello.sh'
+    def printHelloScript = libraryResource 'com/org/scripts/print-hello.sh'
     def name = parameters.name
     //the following line gives me headaches
     sh(printHelloScript(name))
