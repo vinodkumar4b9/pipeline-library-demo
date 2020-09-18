ba40b1cb7ed9bfbd223d8d51bc80459b446a2cc5
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-18 23:21:59 +0530

    Update my_script.groovy

--- vars/my_script.groovy
+++ vars/my_script.groovy
@@ -5,7 +5,4 @@ def my_function(String serverIp, String scriptArgument) {
     // create a file with script_bash content
     writeFile file: './print-hello.sh', text: script_content
     echo "Execute remote script print-hello.sh..."
-    def sshCommand = "ssh username@${serverIp} \'bash -xs\' < ./test.sh ${scriptArgument}"
-    echo "Ssh command is: ${sshCommand}"
-    sh(sshCommand)
 }
