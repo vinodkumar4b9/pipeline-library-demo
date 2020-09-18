c7cf245068956ffdb5eb7427cfd946df319c4c90
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-18 23:23:52 +0530

    Update my_script.groovy

--- vars/my_script.groovy
+++ vars/my_script.groovy
@@ -5,4 +5,5 @@ def my_function(String serverIp, String scriptArgument) {
     // create a file with script_bash content
     writeFile file: './print-hello.sh', text: script_content
     echo "Execute remote script print-hello.sh..."
+    sh "./print-hello.sh"
 }
