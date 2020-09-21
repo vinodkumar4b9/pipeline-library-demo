02d912be31c851047b44df68f4bf6852e2b73ec4
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-09-21 10:45:05 +0530

    Update my_script.groovy

--- vars/my_script.groovy
+++ vars/my_script.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def my_function(String serverIp, String scriptArgument) {
+def my_function() {
     def script_content = libraryResource 'com/org/scripts/print-hello.sh'
     // create a file with script_bash content
     writeFile file: './print-hello.sh', text: script_content
